59eb424723b149c481ee595f778ec3a36ed300b7
Odair Bis Rosa <odairbr07@gmail.com>
2025-06-19 17:28:31 -0300

    Add category toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,39 @@
 'use client'
 
+import { useState } from 'react';
+import { Button } from '@/components/ui/button'
+
 import { books, pokemons } from './components/data'
 import NavBar from "./components/NavBar"
 import ItemListContainer from './components/ItemListContainer'
 
 export default function Home() {
+  const [category, setCategory] = useState('books');
+  const categoryToggle = category == 'books' ? 'pokemons' : 'books'
+
+  const changeType = () => {
+
+    setCategory(categoryToggle);
+  };
+
+  //const decrement = () => {
+  //  setCategory(prev => Math.max(0, prev - 1));
+  //};
+
 
   return (
     <div>
       <NavBar />
-      <ItemListContainer items={books} title={'Livros'} />
-      <ItemListContainer items={pokemons} title={'Pokemons'}/>
+      <div className="text-2xl font-bold">
+        Categoria atual: {categoryToggle}
+      </div>
+
+      <Button onClick={changeType}> {category} </Button>
+      {categoryToggle == 'books'
+        ? <ItemListContainer items={books} title={'Livros'} />
+        : <ItemListContainer items={pokemons} title={'Pokemons'} />
+      }
+
     </div>
   )
 }
